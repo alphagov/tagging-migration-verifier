feae018fe1dd6480a0a4ec2127f7bd2b944eed43
Tijmen Brommet <tijmen@gmail.com>
2015-12-08 13:50:12 +0000

    Fix env variable for script
    This ENV variable is incorrect.

--- lib/download.rb
+++ lib/download.rb
@@ -1,9 +1,9 @@
 class Download
   def self.taggings_from(service, app_name)
-    # In tasks run by Jenkins, APP_DOMAIN will be available and point to the
+    # In tasks run by Jenkins, GOVUK_APP_DOMAIN will be available and point to the
     # preview/staging/production domains.
-    if ENV['APP_DOMAIN']
-      host = "https://#{service}.#{ENV['APP_DOMAIN']}"
+    if ENV['GOVUK_APP_DOMAIN']
+      host = "https://#{service}.#{ENV['GOVUK_APP_DOMAIN']}"
     else
       host = "http://#{service}.dev.gov.uk"
     end
