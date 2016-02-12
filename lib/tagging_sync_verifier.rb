28a542a70ff841389691469cd3825edeb5f21a58
Tijmen Brommet <tijmen@gmail.com>
2016-02-12 12:03:11 +0000

    X

--- lib/tagging_sync_verifier.rb
+++ lib/tagging_sync_verifier.rb
@@ -1,4 +1,5 @@
 require 'json'
+require 'ap'
 require_relative '../lib/comparer'
 require_relative '../lib/download'
 
@@ -18,8 +19,18 @@ class TaggingSyncVerifier
     if comparison.same?
       puts "Taggings for #{app_name} are in sync!"
     else
-      puts "Taggings for #{app_name} are not in sync!"
-      puts comparison.diffs
+      puts "** Taggings for #{app_name} are not in sync! **"
+
+      comparison.diffs[:only_a].each do |content_id, tags|
+        puts "#{content_id} has the following tags in content-api:"
+        ap tags
+      end
+
+      comparison.diffs[:only_b].each do |content_id, tags|
+        puts "#{content_id} has the following tags in content-store:"
+        ap tags
+      end
+
       exit(1)
     end
   end
